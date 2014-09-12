0c69e647f9a63786505574b45bc144ccdb3695e0
John Shearer <john@ultranaut.com>
2014-09-11 23:58:37 -0400

    Add Jade, use to render '/' view

--- client/server.js
+++ client/server.js
@@ -2,11 +2,22 @@
 
 var express = require('express');
 var path = require('path');
+var jade = require('jade');
 var logger = require('morgan');
+
 var app = express();
 
+// log events
 app.use(logger('dev'));
+
+// configure template engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 app.use(express.static(path.join(__dirname, 'public')));
 
+// routing
+app.get('/', function (req, res) {
+  res.render('index');
+});
 
 app.listen(8080);
